00daaf49284d41a6dbced4a22697f4ac13769a18
Soost Icus <soosticus@gmail.com>
2021-01-18 01:10:15 -0800

    initial commit

---
+++ colors.lua
@@ -0,0 +1,30 @@
+local _, core = ...;
+
+core.ClassColors = {
+	  ["Druid"] = { r = 1.00, g = 0.49, b = 0.04, hex = "FF7D0A" },
+	[ "Hunter"] = { r = 0.67, g = 0.83, b = 0.45, hex = "ABD473" },
+	   ["Mage"] = { r = 0.25, g = 0.78, b = 0.92, hex = "40C7EB" },
+	 ["Priest"] = { r = 1.00, g = 1.00, b = 1.00, hex = "FFFFFF" },
+	  ["Rogue"] = { r = 1.00, g = 0.96, b = 0.41, hex = "FFF569" },
+	 ["Shaman"] = { r = 0.00, g = 0.44, b = 0.87, hex = "F58CBA" },
+	["Paladin"] = { r = 0.96, g = 0.55, b = 0.73, hex = "F58CBA" },
+	["Warlock"] = { r = 0.53, g = 0.53, b = 0.93, hex = "8787ED" },
+	["Warrior"] = { r = 0.78, g = 0.61, b = 0.43, hex = "C79C6E" }
+}
+
+-- local tc_classes = {}
+
+-- core.faction = UnitFactionGroup("player")
+-- if core.faction == "Horde" then
+-- 	tc_classes = { "Druid", "Hunter", "Mage", "Priest", "Rogue", "Shaman", "Warlock", "Warrior" }
+-- elseif core.faction == "Alliance" then
+-- 	tc_classes = { "Druid", "Hunter", "Mage", "Paladin", "Priest", "Rogue", "Warlock", "Warrior" }
+-- end
+
+-- core.classes = {}
+-- for i = 1, #tc_classes do
+-- 	local cname = tc_classes[i]
+-- 	local lname = string.upper(cname)
+-- 	core.CColors[lname] = ClassColors[cname]
+-- 	table.insert(core.classes, lname)
+-- end
---
+++ main.lua
@@ -0,0 +1,114 @@
+local _, core = ...;
+
+local AddonPrefix = "PRIOLOOTLIST"
+
+local function listmembers(rank_index)
+    local num_members = GetNumGuildMembers()
+    for index = 1, num_members, 1 do
+        local name, rank, rankIndex, level, class, zone, note, officernote, online, status, classFileName,
+            achievementPoints, achievementRank, isMobile, isSoREligible, standingID = GetGuildRosterInfo(index);
+        if not rank_index or rankIndex == tonumber(rank_index) then
+            local color = core.ClassColors[class]
+            print(string.format("|cff%s%s|r is rank %s", color.hex, name, rank))
+        end
+    end
+end
+
+local function listranks()
+    local num_ranks = GuildControlGetNumRanks()
+    print("Listing " .. num_ranks .. " guild ranks")
+    for index = 1, num_ranks, 1 do
+        local rank = GuildControlGetRankName(index);
+        local color = core.ClassColors[class]
+        print(string.format("%02d: %s", index - 1, rank))
+    end
+end
+
+local function split(inputstr, sep)
+    local sep = sep or '%s'
+    local t = {}
+    for field, s in string.gmatch(inputstr, "([^" .. sep .. "]*)(" .. sep .. "?)") do
+        table.insert(t, field)
+        if s == "" then
+            return t
+        end
+    end
+end
+
+local function process_cmd(funcs, ...)
+    local arg = {...}
+    local func_name = tostring(arg[1])
+    local func_args = {unpack(arg, 2, table.getn(arg))}
+    local func_args_str = "nil"
+    if func_args and table.getn(func_args) then
+        func_args_str = table.concat(func_args, ';')
+    end
+    local func = funcs[func_name]
+    if func then
+        func(unpack(func_args))
+    else
+    end
+end
+
+local function list(...)
+    process_cmd({
+        ["ranks"] = listranks,
+        ["members"] = listmembers
+    }, ...)
+end
+
+local customSoundId = nil
+
+local function stopsound()
+    if(customSoundId ~= nil) then
+        StopSound(customSoundId)
+        customSoundId = nil
+    end
+end
+
+local function chickendrums()
+    local soundType = {
+        SOUND = 1,
+        GAME_MUSIC = 2,
+        CUSTOM = 3
+    }
+    local drums = {
+        ["sound"] = "Interface\\AddOns\\PriorityLootList\\Sounds\\chickendrums.mp3",
+        ["description"] = "Chicken Drums!",
+        ["type"] = soundType.CUSTOM
+    }
+    C_ChatInfo.SendAddonMessage( AddonPrefix, "cd", "RAID" );
+    stopsound()
+    customSoundId = select(2, PlaySoundFile(drums.sound))
+end
+
+function Addon_OnEvent(self, event, ...)
+    if event == "CHAT_MSG_ADDON" then
+        local args = {...}
+        if args[1] == AddonPrefix then
+            -- print(event, ...)
+            if args[2] == "cd" then
+                chickendrums()
+            end
+        end
+	elseif event == "PLAYER_LOGIN" then
+		local successfulRequest = C_ChatInfo.RegisterAddonMessagePrefix(AddonPrefix)
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		-- C_ChatInfo.SendAddonMessage(AddonPrefix, "cd", "WHISPER", UnitName("player"))
+	end
+end
+
+SLASH_PPL1 = "/ppl"
+SlashCmdList["PPL"] = function(cmd)
+    process_cmd({
+        ["list"] = list,
+        ["chickendrums"] = chickendrums,
+        ["stopsound"] = stopsound
+    }, unpack(split(cmd)))
+end
+
+local f = CreateFrame("Frame")
+f:SetScript("OnEvent", Addon_OnEvent)
+f:RegisterEvent("CHAT_MSG_ADDON");
+f:RegisterEvent("PLAYER_LOGIN");
+f:RegisterEvent("PLAYER_ENTERING_WORLD");
