7beb91eff5c005729b2a538d18aa481b850563fe
Soost Icus <soosticus@gmail.com>
2021-01-22 23:52:55 -0800

    minor edits to toc

--- PriorityLootList/main.lua
+++ PriorityLootList/main.lua
@@ -2,6 +2,8 @@ local _G = _G
 _G.PLL = _G.LibStub("AceAddon-3.0"):NewAddon("PLL", "AceConsole-3.0", "AceComm-3.0");
 local PLL = _G.PLL;
 
+local version = "${ADDON_VERSION}";
+
 PLL.customSoundId = nil
 function PLL:StopSound()
     if(PLL.customSoundId ~= nil) then
