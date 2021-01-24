4105e4fa5066dca3f0ded0b88ad77d05b940cd4b
Soost Icus <soosticus@gmail.com>
2021-01-24 02:31:57 -0800

    separate addon init from main

---
+++ PriorityLootList/init.lua
@@ -0,0 +1,6 @@
+local _G = _G
+_G.PLL = _G.LibStub("AceAddon-3.0"):NewAddon("PLL", "AceConsole-3.0", "AceComm-3.0");
+local PLL = _G.PLL;
+
+PLL.name = "Priority Loot List";
+PLL.version = "${ADDON_VERSION}";
